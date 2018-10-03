0e9067db7356c083b46f2fa653d56ab74c64e18c
Boimb <atboimb@gmail.com>
2018-10-03 22:20:57 +0200

    Title and targets

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,11 +5,11 @@ import { Text } from 'preact-i18n';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Preact App</h1>
+		<h1>Gladys Developers</h1>
 		<nav>
 			<Link activeClassName={style.active} href="/"><Text id="generics.home" /></Link>
-			<a href="https://community.gladysproject.com"><Text id="header.forum"/></a>
-			<a href="https://github.com/GladysProject/Gladys">GitHub</a>
+			<a href="https://community.gladysproject.com" target='blank'><Text id="header.forum"/></a>
+			<a href="https://github.com/GladysProject/Gladys" target='blank'>GitHub</a>
 			<Link activeClassName={style.active} href="/login"><Text id="header.connection" /></Link>
 		</nav>
 	</header>
