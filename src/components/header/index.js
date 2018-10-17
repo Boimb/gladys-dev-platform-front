8728b4c698918687199ae91aa70d3849442d740b
Boimb <atboimb@gmail.com>
2018-10-17 23:09:25 +0200

    Added react-feather. Some improvement in ProfileWrapper

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,6 @@
 import ProfileWrapper from './profile-wrapper'
 
-import IconMenu from 'react-icons/lib/md/menu';
-
+import { Menu } from 'react-feather'
 // style
 import './style.scss';
 
@@ -9,7 +8,7 @@ import './style.scss';
 export default (props) => (
   <header className="site-head d-flex align-items-center justify-content-between">
     <div className="wrap mr-4">
-      <IconMenu size="24" color="#fff" onClick={props.toggleNav} style={{cursor: 'pointer'}}/>
+      <Menu size="24" color="#fff" onClick={props.toggleNav} style={{cursor: 'pointer'}}/>
     </div>
     <div className="right-elems ml-auto d-flex">
       <ProfileWrapper {...props}/>
--- src/components/header/profile-wrapper.js
+++ src/components/header/profile-wrapper.js
@@ -8,11 +8,11 @@ import {
 } from 'reactstrap';
 
 // icons
-import IconFace from 'react-icons/lib/md/face';
 import IconMail from 'react-icons/lib/md/mail';
 import IconSecurity from 'react-icons/lib/md/security';
 import IconHelp from 'react-icons/lib/md/help';
-import IconLogout from 'react-icons/lib/md/power-settings-new';
+
+import { User, Info, LogOut } from 'react-feather'
 
 // I18n
 import { Text } from 'preact-i18n'
@@ -31,16 +31,24 @@ export default (props) => (
 
 const ProfileMenu = props => (<UncontrolledDropdown>
   <DropdownToggle tag="div">
-    <img src="http://i.imgur.com/0rVeh4A.jpg" alt="avatar"/>
+    <User size={24} color='white'/>
   </DropdownToggle>
   <DropdownMenu right style={{minWidth: '12rem'}}>
-    <DropdownItem header>Balance: $993.4</DropdownItem>
+    <DropdownItem header>{`Hey! ${props.user.name}`}</DropdownItem>
     <DropdownItem divider/>
-    <DropdownItem><IconFace size="16"/>&emsp;<a href="#na">Profile</a></DropdownItem>
+    <DropdownItem><Info size="16"/> <a href="#na"><Text id={'generics.profile'}/></a></DropdownItem>
     <DropdownItem><IconMail size="16"/>&emsp;<a href="#na">Inbox</a></DropdownItem>
     <DropdownItem><IconSecurity size="16"/>&emsp;<a href="#na">Security</a></DropdownItem>
     <DropdownItem><IconHelp size="16"/>&emsp;<a href="#na">Help</a></DropdownItem>
-    <div className="text-right ml-3 mr-3 mt-2"><Button block color="success" size="sm"><IconLogout size="15"/>&emsp;Logout</Button></div>
+    <div className="text-right ml-3 mr-3 mt-2">
+      <Button block color="success" size="sm" onClick={() => {
+        console.log('clicked')
+        props.logout()
+      }}>
+        <LogOut size="15"/>
+        <Text id='generics.logout'/>
+      </Button>
+    </div>
   </DropdownMenu>
 </UncontrolledDropdown>)
 
