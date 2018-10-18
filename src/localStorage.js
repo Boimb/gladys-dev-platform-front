faa02f8cbd5ec15d86b1df347084abceeab79878
Boimb <atboimb@gmail.com>
2018-10-18 13:24:58 +0200

    Del useless logs / comments

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,27 +1,23 @@
 export const loadState = () => {
-  console.log('loading State')
   try {
     const serializedState = localStorage.getItem('gladys-dev-platform');
     if (serializedState === null) {
       return undefined;
     }
-    console.log('State equals to ', JSON.parse(serializedState))
     return JSON.parse(serializedState);
   } catch (err) {
-    console.warn('loadState err', err)
+    console.warn('loadState err', err);
     // TODO maybe log this somewhere
     return undefined;
   }
 };
 
 export const saveState = (state) => {
-  console.log('saving State')
   try {
-    const serializedState = JSON.stringify(state)
-    localStorage.setItem('gladys-dev-platform', serializedState)
-    console.log('saved State equals to ', state)
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('gladys-dev-platform', serializedState);
   } catch (err) {
-    console.warn('saveState err', err)
+    console.warn('saveState err', err);
     // TODO maybe log this somewhere
   }
-}
+};
--- src/store.js
+++ src/store.js
@@ -1,42 +1,32 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import monitorReducerEnhancer from './enhancers/monitorReducer'
-import rootReducer from './reducers'
-import axios from 'axios'
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import monitorReducerEnhancer from './enhancers/monitorReducer';
+import rootReducer from './reducers';
+import axios from 'axios';
 import axiosMiddleware from 'redux-axios-middleware';
 
 const client = axios.create({ //all axios can be used, shown in axios documentation
-  baseURL:'http://localhost:8080/',
+  baseURL: 'http://localhost:8080/',
   responseType: 'json',
   headers: {
     Authorization: `Bearer ${'token'}`,
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 });
 
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger();
 export default function configureStore (preloadedState) {
 
-  const middlewares = [loggerMiddleware, thunkMiddleware, axiosMiddleware(client)];
+  const middlewares = [
+    loggerMiddleware,
+    thunkMiddleware,
+    axiosMiddleware(client),
+  ];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
-  const enhancers = [middlewareEnhancer, monitorReducerEnhancer]
-  const composedEnhancers = compose(...enhancers)
+  const enhancers = [middlewareEnhancer, monitorReducerEnhancer];
+  const composedEnhancers = compose(...enhancers);
 
-  return createStore(rootReducer, preloadedState, composedEnhancers)
-
-
-  // return createStore(
-  //   combineReducers({
-  //     ...reducers,
-  //     routing: routerReducer
-  //   }),
-  //   preloadedState,
-  //   applyMiddleware(
-  //     thunkMiddleware,
-  //     routerMiddleware(browserHistory),
-  //     loggerMiddleware
-  //   )
-  // )
+  return createStore(rootReducer, preloadedState, composedEnhancers);
 }
