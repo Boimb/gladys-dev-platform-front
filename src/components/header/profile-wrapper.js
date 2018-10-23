ad309c1e27bbfcea5a13cde5e9f8fe449e6f9653
Boimb <atboimb@gmail.com>
2018-10-23 23:05:18 +0200

    Cosmetics

--- src/components/header/profile-wrapper.js
+++ src/components/header/profile-wrapper.js
@@ -1,25 +1,14 @@
-import screenfull from 'screenfull';
-
-import { route } from 'preact-router'
+import { route } from 'preact-router';
 
 import {
-  UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem,
-  Button
+  Button, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown,
 } from 'reactstrap';
 
-// icons
-import IconMail from 'react-icons/lib/md/mail';
-import IconSecurity from 'react-icons/lib/md/security';
-import IconHelp from 'react-icons/lib/md/help';
-
-import { User, Info, LogOut } from 'react-feather'
-
+import { Info, LogOut, User } from 'react-feather';
 // I18n
-import { Text } from 'preact-i18n'
-
+import { Text } from 'preact-i18n';
 // style
 import './style.scss';
-
 
 export default (props) => (
   <div className="wrap profile">
@@ -28,33 +17,38 @@ export default (props) => (
       : <LoginMenu {...props} />}
   </div>);
 
-
-const ProfileMenu = props => (<UncontrolledDropdown>
-  <DropdownToggle tag="div">
-    <User size={24} color='white'/>
-  </DropdownToggle>
-  <DropdownMenu right style={{minWidth: '12rem'}}>
-    <DropdownItem header>{`Hey! ${props.user.name}`}</DropdownItem>
-    <DropdownItem divider/>
-    <DropdownItem onClick={() => route('/profile')}><Info color='black' size="16"/><Text id={'generics.profile'}/></DropdownItem>
-    <DropdownItem><IconMail color='black' size="16"/>&emsp;<a href="#na">Inbox</a></DropdownItem>
-    <DropdownItem><IconSecurity color='black' size="16"/>&emsp;<a href="#na">Security</a></DropdownItem>
-    <DropdownItem><IconHelp color='black' size="16"/>&emsp;<a href="#na">Help</a></DropdownItem>
-    <div className="text-right ml-3 mr-3 mt-2">
-      <Button block color="success" size="sm" onClick={() => {
-        console.log('clicked')
-        props.logout()
-      }}>
-        <LogOut size="15"/>
-        <Text id='generics.logout'/>
-      </Button>
-    </div>
-  </DropdownMenu>
-</UncontrolledDropdown>)
+const ProfileMenu = props => (<div className='profile-menu'>
+  <UncontrolledDropdown>
+    <DropdownToggle tag="div">
+      <User size={24} color='white'/>
+    </DropdownToggle>
+    <DropdownMenu right style={{minWidth: '12rem'}}>
+      <DropdownItem header>{`Hey! ${props.user.name}`}</DropdownItem>
+      <DropdownItem divider/>
+      <DropdownItem onClick={() => route('/profile')}>
+        <Info color='black' size="16"/>
+        <span>
+          <Text id={'generics.profile'}/>
+        </span>
+      </DropdownItem>
+      <div className="text-right ml-3 mr-3 mt-2 signout">
+        <Button block color="success" size="sm" onClick={() => {
+          console.log('clicked');
+          props.logout();
+        }}>
+          <LogOut size="15"/>
+          <span><Text id='generics.logout'/></span>
+        </Button>
+      </div>
+    </DropdownMenu>
+  </UncontrolledDropdown>
+</div>);
 
 const LoginMenu = props => (
   <div>
-    <Button onClick={() => route('/signup')} color="info" outline className="mb-2"><Text id={'header.register'}/></Button>{" "}
-    <Button onClick={() => route('/login')} color="success" className="mb-2"><Text id={'header.login'}/></Button>{" "}
+    <Button onClick={() => route('/signup')} color="info" outline
+            className="mb-2"><Text id={'header.register'}/></Button>{' '}
+    <Button onClick={() => route('/login')} color="success"
+            className="mb-2"><Text id={'header.login'}/></Button>{' '}
   </div>
-)
+);
